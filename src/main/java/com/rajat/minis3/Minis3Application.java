e7f1a1971daf1a7d2d5b769ea76d481659f60e11
rajatbhagat <rajat.bhagat@gmail.com>
2025-12-21 18:32:04 -0600

    Initial Commit

---
+++ src/main/java/com/rajat/minis3/Minis3Application.java
@@ -0,0 +1,13 @@
+package com.rajat.minis3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Minis3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Minis3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rajat/minis3/Minis3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajat.minis3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Minis3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
