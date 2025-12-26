0e59769d548b6e4468105a13bef2e4d8251805e8
rajatbhagat <rajat.bhagat@gmail.com>
2025-12-26 14:00:18 -0600

    Adding initial APIs and adding swagger integration

--- src/main/java/com/rajat/minis3/controller/ObjectController.java
+++ src/main/java/com/rajat/minis3/controller/ObjectController.java
@@ -1,4 +1,15 @@
 package com.rajat.minis3.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/objects/")
 public class ObjectController {
+
+    @GetMapping("/")
+    public String health() {
+        return "Hello World!";
+    }
 }
--- src/main/java/com/rajat/minis3/controller/VersionController.java
+++ src/main/java/com/rajat/minis3/controller/VersionController.java
@@ -1,4 +1,15 @@
 package com.rajat.minis3.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/versions/")
 public class VersionController {
+
+    @GetMapping("/")
+    public String health() {
+        return "Hello World!";
+    }
 }
